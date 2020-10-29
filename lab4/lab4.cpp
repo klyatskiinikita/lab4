a1e566170d24e7edbc3af03a07902ad3815f133d
klyatskiinikita <klyatskiinikita@gmail.com>
2020-10-29 21:22:34 +0200

    Добавьте файлы проекта.

---
+++ lab4/lab4.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include "windows.h"
+
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    double a, b;
+    cout << "Введіть число, більше за 999: ";
+    cin >> a;
+    b = static_cast<int>(a) / 100 % 10;
+    cout << b << endl;
+    cout << "Кількість розрядів сотень в записі цього числа: " << b << endl;
+    system("pause");
+    return 0;
+}
